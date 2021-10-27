ccc9012da0899011518abc95cb2b3e933c13f893
willforde <willforde@gmail.com>
2021-10-27 21:35:22 +0100

    add support for templates in docker-compose file

--- action.py
+++ action.py
@@ -7,13 +7,36 @@ from pathlib import Path
 import subprocess
 import argparse
 import tempfile
+import pathlib
 import sys
 import os
 
 # Intercept the signing-key and convert
 parser = argparse.ArgumentParser()
+parser.add_argument("compose_file", type=pathlib.Path)
+parser.add_argument("--variables", nargs="?", const=None)
 parser.add_argument("--signing-key", required=True)
 parsed_args, passthrough_args = parser.parse_known_args()
+
+# Create final compose file if given file is a compose file template
+compose_file = parsed_args.compose_file
+if compose_file.suffix in [".tpl", ".template"]:
+    file_contents = compose_file.read_text()
+
+    if parsed_args.variables is None:
+        print("The variables argument is required when using a template file.", file=sys.stderr)
+        sys.exit(1)
+
+    # Extract variables from argument
+    variables = dict(map(lambda x: x.split(":"), parsed_args.variables.split(",")))
+    for key, val in variables.items():
+        search_key = f"%%{key}%%"
+        file_contents.replace(search_key, val)
+
+    # Save changed docker file
+    compose_file = parsed_args.compose_file.with_suffix(".yml")
+    with open("w", compose_file) as stream:
+        stream.write(file_contents)
 
 # Private key file
 private_key = tempfile.NamedTemporaryFile(mode="w", delete=False)
@@ -24,6 +47,7 @@ try:
     subprocess.run(
         [
             os.environ["GITHUB_ACTION_PATH"] + "/docker-compose-artifact-gen",
+            compose_file,
             *passthrough_args,
             "--signing-key", private_key.name,
         ],
@@ -31,4 +55,5 @@ try:
     )
 finally:
     Path(private_key.name).unlink(missing_ok=True)
+
 sys.exit(0)
